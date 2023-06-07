55d36c6f93d5597ee4c42a63e4694f195c80fa08
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-06-07 14:02:28 +0900

    Create 0607.js

---
+++ test/0607.js
@@ -0,0 +1,30 @@
+// let odd = 0, even = 0;
+// let n = parseInt();
+
+//  0 <= n <= 1000;
+
+// let result = n % 2 ? odd : even;
+
+// for (i = 0; i <= 1000; i++) {
+//   if (even) {
+//     even += i;
+//   } else {
+//     odd += i;
+//   }
+// }
+
+// console.log("홀수의 합 :", odd);
+// console.log("짝수의 합 :", even);
+// console.log("1부터 n까지의 합 :", odd + even);
+
+
+
+
+var n = parseInt();
+for (i = 1; i <= n; i++) {
+
+    if (i % 2 == 0) {
+        n += i;
+    }
+
+}
