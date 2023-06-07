3505c87508956c07f24952edcc85c19c490f29cc
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-06-07 14:18:55 +0900

    [UPDATE] 짝수의 합

--- test/0607.js
+++ test/0607.js
@@ -19,12 +19,18 @@
 
 
 
+function solution(n) {
+    let answer = 0;
 
-var n = parseInt();
-for (i = 1; i <= n; i++) {
+for (i = 2; i <= n; i+=2) {
 
-    if (i % 2 == 0) {
-        n += i;
-    }
-
+    answer += i;
 }
+    return answer;
+}
+
+
+// function solution(n) {
+//     let count = Math.trunc(n/2)
+//     return count * (count+1)
+// }
--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,3 +1,5 @@
+// 23-06-07 첫 풀이 0:27
+
 function solution(n) {
     let answer = 0;
 
@@ -7,3 +9,4 @@ for (i = 2; i <= n; i+=2) {
 }
     return answer;
 }
+
